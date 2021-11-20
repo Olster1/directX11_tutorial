e6d1cbcd8d3fbc884630f912c8efc1804c8bd88a
makinggamesinc <oliver_marsh@live.com.au>
2021-11-20 14:30:55 +1030

    lesson 5

---
+++ lesson6/shader.hlsl
@@ -0,0 +1,31 @@
+
+cbuffer constants : register(b0)
+{
+    float2 offset;
+    float4 uniformColor;
+};
+
+struct VS_Input {
+    float2 pos : POS;
+    float2 uv : TEX;
+};
+
+struct VS_Output {
+    float4 pos : SV_POSITION;
+    float2 uv : TEXCOORD;
+    float4 color : COLOR;
+};
+
+VS_Output vs_main(VS_Input input)
+{
+    VS_Output output;
+    output.pos = float4(input.pos + offset, 0.0f, 1.0f);
+    output.uv = input.uv;
+    output.color = uniformColor;
+    return output;
+}
+
+float4 ps_main(VS_Output input) : SV_Target
+{
+    return input.color;
+}
